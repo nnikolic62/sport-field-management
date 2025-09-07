766be7b670802592d76a230c6e3410572c932100
Nemanja <nemanja.nikolic98@hotmail.com>
2025-09-07 18:55:07 +0200

    init

---
+++ sport-field-management/src/main/java/com/springboot/sport_field_management/SportFieldManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sport_field_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportFieldManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportFieldManagementApplication.class, args);
+	}
+
+}
